a930de8c2eaa93526fcf15f9a8ff38f054142ecc
Vania Almeida <vania.almeida28@hotmail.com>
2020-09-16 06:37:25 -0300

    🚀 feat: class-01 - variables

---
+++ bytebank/src/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Bem vindo ao Bytebank")
+
+    val holder = "Vania Almeida"
+    val accountNumber = 12345
+
+    var balance: Double = 100 + 2.0
+    balance += 100
+    println("titular $holder")
+    println("número da conta $accountNumber")
+    println("saldo $balance")
+}
