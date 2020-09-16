face269d8d5d2ea726b657116afe30aaaf798175
Vania Almeida <vania.almeida28@hotmail.com>
2020-09-16 06:49:58 -0300

    🚀 feat: class-02 - conditions

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -6,7 +6,42 @@ fun main() {
 
     var balance: Double = 100 + 2.0
     balance += 100
-    println("titular $holder")
-    println("número da conta $accountNumber")
-    println("saldo $balance")
+    println("titular: $holder")
+    println("número da conta: $accountNumber")
+    println("saldo: $balance")
+
+    testCondition(balance)
+
+
+    for (i in 1..3) {
+        print("$i ")
+    }
+
+    for (i in 5 downTo 1) {
+        print("$i ")
+    }
+
+    println("ímpares")
+    for (i in 1..10 step 2) {
+        print("$i ")
+    }
+    var i = 0
+    while (i < 3) {
+        if (i == 2) {
+            break
+        } else if (i == 1) {
+            continue
+        }
+        println("while $i")
+        i++
+    }
+
+}
+
+fun testCondition(balance: Double) {
+    when {
+        balance > 0.0 -> println("conta é positiva")
+        balance == 0.0 -> println("conta é neutra")
+        else -> println("conta negativa")
+    }
 }
